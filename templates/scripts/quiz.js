8ae3c41f6b5117ef1493d82853486af92adb4be5
SlowestLoris <zhangraymond05@gmail.com>
2019-11-17 20:57:24 -0500

    Update design, finish quiz implementation

--- templates/scripts/quiz.js
+++ templates/scripts/quiz.js
@@ -1,167 +1,228 @@
-(function() {
-    var questions = [{
-      question: "What is 2*5?",
-      choices: [2, 5, 10, 15, 20],
-      correctAnswer: 2
-    }, {
-      question: "What is 3*6?",
-      choices: [3, 6, 9, 12, 18],
-      correctAnswer: 4
-    }, {
-      question: "What is 8*9?",
-      choices: [72, 99, 108, 134, 156],
-      correctAnswer: 0
-    }, {
-      question: "What is 1*7?",
-      choices: [4, 5, 6, 7, 8],
-      correctAnswer: 3
-    }, {
-      question: "What is 8*8?",
-      choices: [20, 30, 40, 50, 64],
-      correctAnswer: 4
-    }];
-    
-    var questionCounter = 0; //Tracks question number
-    var selections = []; //Array containing user choices
-    var quiz = $('#quiz'); //Quiz div object
-    
-    // Display initial question
+(function () {
+  var questions = [{
+    question: "There are some personal questions in this quiz. Continue at your own will.",
+    choices: ["Continue"],
+    correctAnswer: 0
+  }, {
+    question: "Do you know what problems your partner is currently facing?",
+    choices: ["Yes", "No"],
+    correctAnswer: 0
+  }, {
+    question: "Do you discuss life-changing decisions before making them?",
+    choices: ["Yes", "No"],
+    correctAnswer: 4
+  }, {
+    question: "Is your relationship completely truthful?",
+    choices: ["Yes", "No"],
+    correctAnswer: 0
+  }, {
+    question: "Are there any issues which you have been unable to resolve? ",
+    choices: ["Yes", "No"],
+    correctAnswer: 3
+  }, {
+    question: "Were there any societal influences preventing your relationship?",
+    choices: ["Yes", "No"],
+    correctAnswer: 4
+  }, {
+    question: "Is there a significant age difference between you and your partner? ",
+    choices: ["Yes", "No"],
+    correctAnswer: 4
+  }, {
+    question: "Do you feel as though there are unfair expectations placed upon you?",
+    choices: ["Yes", "No"],
+    correctAnswer: 4
+  }, {
+    question: "Do your parents approve of your relationship?",
+    choices: ["Yes", "No"],
+    correctAnswer: 4
+  }, {
+    question: "Do you have fond memories of when you first met your partner?",
+    choices: ["Yes", "No"],
+    correctAnswer: 4
+  }];
+
+  var questionCounter = 0; //Tracks question number
+  var selections = []; //Array containing user choices
+  var quiz = $('#quiz'); //Quiz div object
+
+  // Display initial question
+  displayNext();
+
+  // Click handler for the 'next' button
+  $('#next').on('click', function (e) {
+    e.preventDefault();
+
+    // Suspend click listener during fade animation
+    if (quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+
+    // If no user selection, progress is stopped
+    if (isNaN(selections[questionCounter])) {
+      alert('Please make a selection!');
+    } else {
+      questionCounter++;
+      displayNext();
+    }
+  });
+
+  // Click handler for the 'prev' button
+  $('#prev').on('click', function (e) {
+    e.preventDefault();
+
+    if (quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+    questionCounter--;
     displayNext();
-    
-    // Click handler for the 'next' button
-    $('#next').on('click', function (e) {
-      e.preventDefault();
-      
-      // Suspend click listener during fade animation
-      if(quiz.is(':animated')) {        
-        return false;
-      }
-      choose();
-      
-      // If no user selection, progress is stopped
-      if (isNaN(selections[questionCounter])) {
-        alert('Please make a selection!');
-      } else {
-        questionCounter++;
-        displayNext();
-      }
-    });
-    
-    // Click handler for the 'prev' button
-    $('#prev').on('click', function (e) {
-      e.preventDefault();
-      
-      if(quiz.is(':animated')) {
-        return false;
-      }
-      choose();
-      questionCounter--;
-      displayNext();
-    });
-    
-    // Click handler for the 'Start Over' button
-    $('#start').on('click', function (e) {
-      e.preventDefault();
-      
-      if(quiz.is(':animated')) {
-        return false;
-      }
-      questionCounter = 0;
-      selections = [];
-      displayNext();
-      $('#start').hide();
-    });
-    
-    // Animates buttons on hover
-    $('.button').on('mouseenter', function () {
-      $(this).addClass('active');
-    });
-    $('.button').on('mouseleave', function () {
-      $(this).removeClass('active');
-    });
-    
-    // Creates and returns the div that contains the questions and 
-    // the answer selections
-    function createQuestionElement(index) {
-      var qElement = $('<div>', {
-        id: 'question'
-      });
-      
-      var header = $('<h2>Question ' + (index + 1) + ':</h2>');
-      qElement.append(header);
-      
-      var question = $('<p>').append(questions[index].question);
-      qElement.append(question);
-      
-      var radioButtons = createRadios(index);
-      qElement.append(radioButtons);
-      
-      return qElement;
+  });
+
+  // Click handler for the 'Start Over' button
+  $('#start').on('click', function (e) {
+    e.preventDefault();
+
+    if (quiz.is(':animated')) {
+      return false;
     }
-    
-    // Creates a list of the answer choices as radio inputs
-    function createRadios(index) {
-      var radioList = $('<ul>');
-      var item;
-      var input = '';
-      for (var i = 0; i < questions[index].choices.length; i++) {
-        item = $('<li>');
-        input = '<input type="radio" name="answer" value=' + i + ' />';
-        input += questions[index].choices[i];
-        item.append(input);
-        radioList.append(item);
-      }
-      return radioList;
+    questionCounter = 0;
+    selections = [];
+    displayNext();
+    $('#start').hide();
+  });
+
+  // Animates buttons on hover
+  $('.button').on('mouseenter', function () {
+    $(this).addClass('active');
+  });
+  $('.button').on('mouseleave', function () {
+    $(this).removeClass('active');
+  });
+
+  // Creates and returns the div that contains the questions and 
+  // the answer selections
+  function createQuestionElement(index) {
+    var qElement = $('<div>', {
+      id: 'question'
+    });
+
+    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+    qElement.append(header);
+
+    var question = $('<p>').append(questions[index].question);
+    qElement.append(question);
+
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+
+    return qElement;
+  }
+
+  // Creates a list of the answer choices as radio inputs
+  function createRadios(index) {
+    var radioList = $('<ul>');
+    var item;
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+      item = $('<li>');
+      input = '<input type="radio" name="answer" value=' + i + ' />';
+      input += questions[index].choices[i];
+      item.append(input);
+      radioList.append(item);
     }
-    
-    // Reads the user selection and pushes the value to an array
-    function choose() {
-      selections[questionCounter] = +$('input[name="answer"]:checked').val();
-    }
-    
-    // Displays next requested element
-    function displayNext() {
-      quiz.fadeOut(function() {
-        $('#question').remove();
-        
-        if(questionCounter < questions.length){
-          var nextQuestion = createQuestionElement(questionCounter);
-          quiz.append(nextQuestion).fadeIn();
-          if (!(isNaN(selections[questionCounter]))) {
-            $('input[value='+selections[questionCounter]+']').prop('checked', true);
-          }
-          
-          // Controls display of 'prev' button
-          if(questionCounter === 1){
-            $('#prev').show();
-          } else if(questionCounter === 0){
-            
-            $('#prev').hide();
-            $('#next').show();
-          }
-        }else {
-          var scoreElem = displayScore();
-          quiz.append(scoreElem).fadeIn();
-          $('#next').hide();
+    return radioList;
+  }
+
+  // Reads the user selection and pushes the value to an array
+  function choose() {
+    selections[questionCounter] = +$('input[name="answer"]:checked').val();
+  }
+
+  // Displays next requested element
+  function displayNext() {
+    quiz.fadeOut(function () {
+      $('#question').remove();
+
+      if (questionCounter < questions.length) {
+        var nextQuestion = createQuestionElement(questionCounter);
+        quiz.append(nextQuestion).fadeIn();
+        if (!(isNaN(selections[questionCounter]))) {
+          $('input[value=' + selections[questionCounter] + ']').prop('checked', true);
+        }
+
+        // Controls display of 'prev' button
+        if (questionCounter === 1) {
+          $('#prev').show();
+        } else if (questionCounter === 0) {
+
           $('#prev').hide();
-          $('#start').show();
+          $('#next').show();
         }
-      });
-    }
-    
-    // Computes score and returns a paragraph element to be displayed
-    function displayScore() {
-      var score = $('<p>',{id: 'question'});
-      
-      var numCorrect = 0;
-      for (var i = 0; i < selections.length; i++) {
-        if (selections[i] === questions[i].correctAnswer) {
-          numCorrect++;
-        }
+      } else {
+        var scoreElem = displayScore();
+        quiz.append(scoreElem).fadeIn();
+        $('#next').hide();
+        $('#prev').hide();
+        $('#start').show();
       }
-      
-      score.append('You got ' + numCorrect + ' questions out of ' +
-                   questions.length + ' right!!!');
-      return score;
+    });
+  }
+
+  //CHANGE THIS HELLO THERE
+  // Computes score and returns a paragraph element to be displayed
+  function displayScore() {
+    var score = $('<p>', { id: 'question' });
+
+    var hamlethoratio = 0;
+    var hamletgertrude = 0;
+    var hamletophelia = 0;
+    
+    //Caring
+    if(selections[1]==0) hamlethoratio++;
+    else hamletgertrude++;
+
+    //Trust
+    if(selections[2]==0) hamlethoratio++;
+    
+    //Loyalty
+    if(selections[3]==0) hamlethoratio++;
+    
+    //Conflictual
+    if(selections[4]==0) {
+      hamletgertrude++;
+      hamletophelia++;
+    } else hamlethoratio++;
+
+    //Forbidden Love
+    if(selections[5]==0) hamletgertrude++;
+
+    //Familial Love
+    if(selections[6]==0) hamletgertrude++;
+
+    //Unrealistic Expecations
+    if(selections[7]==0) hamletophelia++;
+
+    //disapproving parents
+    if(selections[8]==0) hamletophelia++;
+
+    //innocent relationship
+    if(selections[9]==0) {
+      hamlethoratio++;
+      hamletophelia++;
     }
-  })();
+
+    /*
+    for (var i = 1; i < selections.length; i++) {
+      if (selections[i] === questions[i].correctAnswer) {
+        numCorrect++;
+      }
+    }*/
+
+    var maxi = Math.max(hamletgertrude,hamlethoratio,hamletophelia);
+    if(maxi==hamletgertrude) score.append('Your relationship is similar to that of Hamlet and Gertrude!');
+    else if(maxi==hamletophelia) score.append('Your relationship is similar to that of Hamlet and Ophelia!');
+    else score.append('Your relationship is similar to that of Hamlet and Horatio!')
+    return score;
+  }
+})();
